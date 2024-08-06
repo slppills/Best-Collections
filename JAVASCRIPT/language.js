545ecfedc22fb481f6a23486cefa40b2e778bd6b
derek0k <derek0kim@gmail.com>
2024-08-06 13:06:26 +0900

    feat: integrate language selection with movie fetching

--- JAVASCRIPT/language.js
+++ JAVASCRIPT/language.js
@@ -1,21 +1,16 @@
 import { state } from "./state.js";
 import { homeWrapper, footerSpan, fetchAndDisplayMovies } from "./index.js";
 
-// 언어 변경(한국어 or 영어)
-export const changeLanguage = () => {
+// 언어 변경( 한국어 - 일본어 - 영어 )
+export const changeLanguage = (selectedLanguage) => {
   state.scrollPage = 1;
-  state.isLanguageKorean = !state.isLanguageKorean;
   homeWrapper.innerHTML = "";
   footerSpan.style.visibility = "visible";
   state.ifSearching === false
     ? fetchAndDisplayMovies(
-        `https://api.themoviedb.org/3/movie/${state.category}?language=${
-          state.isLanguageKorean ? "ko-KR" : "en-UN"
-        }&page=${state.scrollPage}`
+        `https://api.themoviedb.org/3/movie/${state.category}?language=${selectedLanguage}&page=${state.scrollPage}`
       )
     : fetchAndDisplayMovies(
-        `https://api.themoviedb.org/3/search/movie?query=${state.prevSearchTitle}&include_adult=true&language=${
-          state.isLanguageKorean ? "ko-KR" : "en-UN"
-        }&page=${state.scrollPage}`
+        `https://api.themoviedb.org/3/search/movie?query=${state.prevSearchTitle}&include_adult=true&language=${selectedLanguage}&page=${state.scrollPage}`
       );
 };
